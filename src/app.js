6a471c3df9eded9d71309245084888e05229612d
John Schalk <johnschalk@Johns-MacBook-Pro.local>
2019-11-29 14:51:28 -0600

    fixed profile

--- src/app.js
+++ src/app.js
@@ -1,44 +1,26 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-const express = require("express");
-const app = express();
+const express = require('express');
+const app = new express();
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('views',path.join(__dirname, '/views'));
+app.set('view engine','ejs');
 
+app.use(express.static(path.join(__dirname, '/public')));
 
-const accountData = fs.readFileSync(
-    path.join(__dirname, 'json', 'accounts.json'), 'utf8'
-);
+const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
 const accounts = JSON.parse(accountData);
 
-const userData = fs.readFileSync(
-    path.join(__dirname, 'json', 'accounts.json'), 'utf8'
-);
-
+const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
 const users = JSON.parse(userData);
 
-app.get('/', (req, res) => {
-    res.render('index', {title: 'Account Summary', accounts});
-});
+app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts: accounts }));
 
+app.get('/savings', (req, res) => res.render('account', { account: accounts.savings }));
+app.get('/checking', (req, res) => res.render('account', { account: accounts.checking }));
+app.get('/credit', (req, res) => res.render('account', { account: accounts.credit }));
 
-app.get('/savings', (req, res) => {
-    res.render('account', {account: accounts.savings});
-});
+app.get('/profile', (req, res) =>  res.render('profile', { user: users[0] }));
 
-app.get('/checking', (req, res) => {
-    res.render('account', {account: accounts.checking});
-});
-
-app.get('/credit', (req, res) => {
-    res.render('account', {account: accounts.credit});
-});
-
-app.get('/profile', (req, res) => {
-    res.render('profile', {user: users[0]});
-});
-
-app.listen(3000, () =>  console.log('Project running on port 3000'));
+app.listen(3000, () => { console.log('PS Project Running on port 3000!') });
